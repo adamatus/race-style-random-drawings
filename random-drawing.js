27d64f1167e1c10f2329d481c9b185e6a6bf0971
Adam Riggall <adam@riggall.info>
2013-11-06 18:35:54 -0600

    Major code reorganization
    Move functions into logical order, not false dependency order
    Put all globals at top
    Put UI event binds at bottom

--- random-drawing.js
+++ random-drawing.js
@@ -1,4 +1,6 @@
-var icon_list = {
+var i,j,
+    startDir = 'left',
+    icon_list = {
   bike: {file: 'icons/road_bike.svg', x: 575, y: 312, name: 'Road Bike'},
   car: {file: 'icons/race_car.svg', x: 503, y: 182, name: 'Race Car'},
   pony: {file: 'icons/rocking_horse.svg', x: 570, y: 500, name: 'Pony'},
@@ -7,68 +9,85 @@ var icon_list = {
   // seahorse
   // robot
   // zombie
-};
+},
+  colorList = [
+  {value: "#1F78B4", name: 'Blue', active: 1},
+  {value: "#33A02C", name: 'Green', active: 1},
+  {value: "#E31A1C", name: 'Red', active: 1},
+  {value: "#FF7F00", name: 'Orange', active: 1},
+  {value: "#6A3D9A", name: 'Purple', active: 1},
+  {value: "#A6CEE3", name: 'Light Blue', active: 1},
+  {value: "#B2DF8A", name: 'Light Green', active: 1},
+  {value: "#FB9A99", name: 'Light Red', active: 1},
+  {value: "#FDBF6F", name: 'Light Orange', active: 1},
+  {value: "#CAB2D6", name: 'Light Purple', active: 1}
+],
+    ponies = [],
+    jockeys = [],
+    random = false,
+    ponyOrder = 'sorted',
+    nextPlace = 1,
+    raceLength = 10, // Race length in seconds
+    running = false,
+    done = false,
+    allowChanging = true,
+    advOptionsHidden = true,
+    width = $('#right-col').width(),
+    height = $(window).height()-100,
+    xmax = 110,
+    margins = [50, 30, 50, 30],
+    mb = margins[0],
+    ml = margins[1],
+    mt = margins[2],
+    mr = margins[3],
+    w = width - (ml + mr),
+    h = height - (mb + mt),
+    x = d3.scale.linear().range([0, w]).domain([0,xmax]),
+    y = d3.scale.linear().range([0, h]).domain([0,1]);
+
+// Setup initial SVG chart
+var svg = d3.select('#chart')
+  .append('svg:svg')
+    .attr('class', 'chart')
+    .attr("width", width)
+    .attr("height", height);
+
+var defs = svg.append('defs');
+var plot = svg.append('g')
+  .attr('id','plot')
+  .attr('transform','translate('+ml+','+mt+')');
+var dataRegion = plot.append('g')
+  .attr('id','data-region');
+
+// Add the start and finish lines
+d3.select('#data-region').selectAll('.border-line')
+    .data([10, 100],function(d) { return d; })
+  .enter()
+    .append('svg:line')
+      .classed('border-line',true)
+      .attr('x1',function(d) { return x(d); })
+      .attr('x2',function(d) { return x(d); })
+      .attr('y1',y(0))
+      .attr('y2',y(1))
+      .style('stroke','black')
+      .style('stroke-dasharray','10,10');
+
+// Generic helper functions
 
 var getRandomInt = function(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+// Specific helper functions
+
 var getStepSize = function() {
   return (90*ponies.length*50)/(raceLength*1000);
 };
 
-var runPonies = function() {
-  if (running) {
-    // Figure out which (if any) ponies haven't finished
-    var notFinished = [];
-    for (var i = 0; i < ponies.length; i++) {
-      if (ponies[i].progress < 90) {
-        notFinished.push(i);
-      }
-    }
-
-    // See if any of the ponies still need to finish
-    if (notFinished.length > 0) {
-      // Move a random (still running) pony forward and record if they
-      // made it to the end
-      var tmp = getRandomInt(0,notFinished.length-1);
-      ponies[notFinished[tmp]].progress += getStepSize();
-      if (ponies[notFinished[tmp]].progress >= 90)
-      {
-        ponies[notFinished[tmp]].finishPos = nextPlace++;
-      }
-
-      // Actually move the pony
-      d3.selectAll('.pony')
-          .data(ponies,function(d) { return d.jockey + d.num; })
-        .transition().ease('linear').duration(50)
-          .attr('transform',function(d,i) {
-            return 'translate('+x(d.progress)+',0)';
-          });
-      setTimeout(runPonies,50);
-    } else {
-      d3.selectAll('.rank-entry').remove();
-      d3.select('#winner-list').selectAll('.rank-entry')
-          .data(ponies,function(d) { return d.finishPos; })
-        .enter().append('p')
-          .classed('rank-entry',true)
-          .text(function(d,i) { return d.finishPos + ': ' + d.jockey;}) ;
-
-      d3.selectAll('.rank-entry').sort(function(a,b) {
-        return a.finishPos < b.finishPos ? -1 : 1;
-      })
-        .order();
-
-      $('#winner-list-modal').modal('show');
-
-      $("#go-button").text('Done');
-      done = true;
-      running = false;
-    }
-  }
+var updateSpeed = function() {
+  raceLength = 21-$('#race-speed').val();
+  console.log(raceLength);
 };
-
-var i,j;
 
 var addEntrant = function() {
   if (allowChanging) {
@@ -185,6 +204,57 @@ var removeEntrant = function(i) {
   }
 };
 
+var runPonies = function() {
+  if (running) {
+    // Figure out which (if any) ponies haven't finished
+    var notFinished = [];
+    for (var i = 0; i < ponies.length; i++) {
+      if (ponies[i].progress < 90) {
+        notFinished.push(i);
+      }
+    }
+
+    // See if any of the ponies still need to finish
+    if (notFinished.length > 0) {
+      // Move a random (still running) pony forward and record if they
+      // made it to the end
+      var tmp = getRandomInt(0,notFinished.length-1);
+      ponies[notFinished[tmp]].progress += getStepSize();
+      if (ponies[notFinished[tmp]].progress >= 90)
+      {
+        ponies[notFinished[tmp]].finishPos = nextPlace++;
+      }
+
+      // Actually move the pony
+      d3.selectAll('.pony')
+          .data(ponies,function(d) { return d.jockey + d.num; })
+        .transition().ease('linear').duration(50)
+          .attr('transform',function(d,i) {
+            return 'translate('+x(d.progress)+',0)';
+          });
+      setTimeout(runPonies,50);
+    } else {
+      d3.selectAll('.rank-entry').remove();
+      d3.select('#winner-list').selectAll('.rank-entry')
+          .data(ponies,function(d) { return d.finishPos; })
+        .enter().append('p')
+          .classed('rank-entry',true)
+          .text(function(d,i) { return d.finishPos + ': ' + d.jockey;}) ;
+
+      d3.selectAll('.rank-entry').sort(function(a,b) {
+        return a.finishPos < b.finishPos ? -1 : 1;
+      })
+        .order();
+
+      $('#winner-list-modal').modal('show');
+
+      $("#go-button").text('Done');
+      done = true;
+      running = false;
+    }
+  }
+};
+
 var resetPonies = function() {
   // Send the ponies back to the beginning
   for (i = 0; i < ponies.length; i++) {
@@ -207,46 +277,6 @@ var resetPonies = function() {
   d3.selectAll('.rank-entry').remove();
 };
 
-var ponyOrder = 'sorted';
-
-var toggleRandomOrder = function() {
-  // TODO Change button style/activate
-  ponyOrder = ponyOrder == 'sorted' ? 'random' : 'sorted';
-
-  if (ponyOrder === "sorted") {
-    // Sort ponies back to jockey order
-    neworder = [];
-    for (i=0; i < jockeys.length; i++) {
-      for (j=0; j < ponies.length; j++) {
-        if (jockeys[i].name == ponies[j].jockey) {
-          neworder.push(j);
-        }
-      }
-    }
-    newponies = [];
-    for (j=0; j < ponies.length; j++) {
-      newponies.push(ponies[neworder[j]]);
-    }
-    ponies = newponies;
-  } else {
-    // Shuffle the ponies, using code from here:
-    // http://stackoverflow.com/a/2450976
-    var currentIndex = ponies.length, temporaryValue, randomIndex;
-
-     // While there remain elements to shuffle...
-     while (0 !== currentIndex) {
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-
-      // And swap it with the current element.
-      temporaryValue = ponies[currentIndex];
-      ponies[currentIndex] = ponies[randomIndex];
-      ponies[randomIndex] = temporaryValue;
-    }
-  }
-  updatePonyLineup();
-};
 
 var updateJockeyLineup = function() {
 
@@ -394,7 +424,44 @@ var expandAdvOptions = function() {
   }
 };
 
-var startDir = 'left';
+var toggleRandomOrder = function() {
+  // TODO Change button style/activate
+  ponyOrder = ponyOrder == 'sorted' ? 'random' : 'sorted';
+
+  if (ponyOrder === "sorted") {
+    // Sort ponies back to jockey order
+    neworder = [];
+    for (i=0; i < jockeys.length; i++) {
+      for (j=0; j < ponies.length; j++) {
+        if (jockeys[i].name == ponies[j].jockey) {
+          neworder.push(j);
+        }
+      }
+    }
+    newponies = [];
+    for (j=0; j < ponies.length; j++) {
+      newponies.push(ponies[neworder[j]]);
+    }
+    ponies = newponies;
+  } else {
+    // Shuffle the ponies, using code from here:
+    // http://stackoverflow.com/a/2450976
+    var currentIndex = ponies.length, temporaryValue, randomIndex;
+
+     // While there remain elements to shuffle...
+     while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = ponies[currentIndex];
+      ponies[currentIndex] = ponies[randomIndex];
+      ponies[randomIndex] = temporaryValue;
+    }
+  }
+  updatePonyLineup();
+};
 
 var toggleStartDir = function() {
   startDir = startDir == 'left' ? 'right' : 'left';
@@ -416,22 +483,6 @@ var processEnter = function(e) {
     e.preventDefault();
   }
 };
-
-$('input[id=num]').on('keypress',function(e) { return processEnter(e); });
-$('input[id=name]').on('keypress',function(e) { return processEnter(e); });
-
-var colorList = [
-  {value: "#1F78B4", name: 'Blue', active: 1},
-  {value: "#33A02C", name: 'Green', active: 1},
-  {value: "#E31A1C", name: 'Red', active: 1},
-  {value: "#FF7F00", name: 'Orange', active: 1},
-  {value: "#6A3D9A", name: 'Purple', active: 1},
-  {value: "#A6CEE3", name: 'Light Blue', active: 1},
-  {value: "#B2DF8A", name: 'Light Green', active: 1},
-  {value: "#FB9A99", name: 'Light Red', active: 1},
-  {value: "#FDBF6F", name: 'Light Orange', active: 1},
-  {value: "#CAB2D6", name: 'Light Purple', active: 1}
-];
 
 var updatePicker = function() {
   // Add the color picker
@@ -455,50 +506,6 @@ var updatePicker = function() {
   $('select[name="colorpicker"]').simplecolorpicker({picker: true});
 };
 
-updatePicker();
-
-var updateSpeed = function() {
-  raceLength = 21-$('#race-speed').val();
-  console.log(raceLength);
-};
-
-var ponies = [];
-var jockeys = [];
-var random = false;
-
-var nextPlace = 1;
-var raceLength = 10; // Race length in seconds
-
-var running = false;
-var done = false;
-var allowChanging = true;
-var advOptionsHidden = true;
-
-var width = $('#right-col').width();
-    height = $(window).height()-100;
-
-var xmax = 110;
-
-var margins = [50, 30, 50, 30],
-    mb = margins[0], ml = margins[1], mt = margins[2], mr = margins[3];
-var w = width - (ml + mr),
-    h = height - (mb + mt);
-var x = d3.scale.linear().range([0, w]).domain([0,xmax]);
-var y = d3.scale.linear().range([0, h]).domain([0,1]);
-
-// Add chart SVG
-var svg = d3.select('#chart')
-  .append('svg:svg')
-    .attr('class', 'chart')
-    .attr("width", width)
-    .attr("height", height);
-
-var defs = svg.append('defs');
-var plot = svg.append('g')
-  .attr('id','plot')
-  .attr('transform','translate('+ml+','+mt+')');
-var dataRegion = plot.append('g')
-  .attr('id','data-region');
 
 var runButtonClick = function() {
   // Only run ponies if there is at least one to run!
@@ -527,19 +534,6 @@ var resetButtonClick = function() {
   resetPonies();
 };
 
-// Add the start and finish lines
-d3.select('#data-region').selectAll('.border-line')
-    .data([10, 100],function(d) { return d; })
-  .enter()
-    .append('svg:line')
-      .classed('border-line',true)
-      .attr('x1',function(d) { return x(d); })
-      .attr('x2',function(d) { return x(d); })
-      .attr('y1',y(0))
-      .attr('y2',y(1))
-      .style('stroke','black')
-      .style('stroke-dasharray','10,10');
-
 $(window).resize(function() {
   // TODO Add in limits at which points we don't get smaller than
   width = $('#right-col').width();
@@ -562,15 +556,6 @@ $(window).resize(function() {
     .attr('y2',y(ponies.length > 0 ? ponies.length : 1));
 
   updatePonyLineup();
-});
-
-// Advance option toggle buttons
-$("#race-dir-toggle").on( "click", function() {
-  toggleStartDir();
-});
-
-$("#randomize-toggle").on("click", function() {
-  toggleRandomOrder();
 });
 
 // Add pop-up winner dialog
@@ -611,5 +596,19 @@ var addWinnerDialog = function() {
       .text('Close');
 };
 
-addWinnerDialog();
+////// Things to run on page load
 
+$('input[id=num]').on('keypress',function(e) { return processEnter(e); });
+$('input[id=name]').on('keypress',function(e) { return processEnter(e); });
+
+// Advance option toggle buttons
+$("#race-dir-toggle").on( "click", function() {
+  toggleStartDir();
+});
+
+$("#randomize-toggle").on("click", function() {
+  toggleRandomOrder();
+});
+
+updatePicker();
+addWinnerDialog();
